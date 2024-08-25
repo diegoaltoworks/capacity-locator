23c9375edabb7e6c5f130f42a62ea7c5fdc0f68b
Diego Monte Alto <diego@diegoalto.co.uk>
2024-08-25 10:48:01 +0100

    fix: :rocket: Terraform fixes
    fixed terraform docker image name; added gcp_credentials variable declaration

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,11 @@ variable "REGION" { type = string }
 variable "DB_USER" { type = string }
 variable "DB_PASSWORD" { type = string }
 variable "DB_NAME" { type = string }
+variable "gcp_credentials" {
+  type        = string
+  sensitive   = true
+  description = "Google Cloud service account credentials"
+}
 
 # Provider configuration
 terraform {
